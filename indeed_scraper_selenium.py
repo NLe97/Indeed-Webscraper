a765b746e7b0eecf4f27b94a12cad6c70270adc0
Ben Le <lebenjamin77@yahoo.com>
2022-05-06 09:17:48 -0600

    selenium.py edit

--- indeed_scraper_selenium.py
+++ indeed_scraper_selenium.py
@@ -11,3 +11,14 @@ def get_url(position, location):
     location = location.replace(' ', '+')
     url = template.format(position, location)
     return url
+
+def get_record(card):
+    """Extract job data from single card"""
+    job_title = card.find_element_by_class_name('jobtitle').text
+    company = card.find_element_by_class_name('company').text
+    location = card.find_element_by_class_name('location').text
+    post_date = card.find_element_by_class_name('date').text
+    extract_date = datetime.today().strftime('%Y-%m-%d')
+    summary = card.find_element_by_class_name('summary').text
+    job_url = card.find_element_by_class_name('jobtitle').get_attribute('href')
+    return job_title, company, location, post_date, extract_date, summary, job_url
