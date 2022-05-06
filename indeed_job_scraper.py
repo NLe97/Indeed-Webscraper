f85d810fab38e5eba15de373559113f6b4657b1f
Ben Le <lebenjamin77@yahoo.com>
2022-05-06 01:56:59 -0600

    adding initial commit import to projects

---
+++ indeed_job_scraper.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+from random import random
+from time import sleep
+from email.message import EmailMessage
+from collections import namedtuple
+import smtplib
+import csv
